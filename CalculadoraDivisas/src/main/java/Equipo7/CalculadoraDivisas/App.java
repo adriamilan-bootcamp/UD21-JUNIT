70d28839047a5e3d700e4559c4b2e7859cac77bc
Adrià Milan <adria4milan@gmail.com>
2022-08-05 21:20:22 +0200

    CalculadoraDivisas Initial

---
+++ CalculadoraDivisas/src/main/java/Equipo7/CalculadoraDivisas/App.java
@@ -0,0 +1,13 @@
+package Equipo7.CalculadoraDivisas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
