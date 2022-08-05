45e3629a1f7b43c4b425059dc560b568d795c68e
Adrià Milan <adria4milan@gmail.com>
2022-08-05 09:48:57 +0200

    areaCuadrado Test v1.1

--- Junit09_Geometria/src/test/java/JUnit/TestJunit09_Geometria/GeometriaTest.java
+++ Junit09_Geometria/src/test/java/JUnit/TestJunit09_Geometria/GeometriaTest.java
@@ -1,7 +1,24 @@
 package JUnit.TestJunit09_Geometria;
+
 import org.junit.jupiter.api.Test;
 import JUnit.Junit09_Geometria.dto.Geometria;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 public class GeometriaTest {
-
+	
+	Geometria geometria;
+	
+	@BeforeEach
+	public void before() {
+		geometria = new Geometria();
+	}
+	
+	@Test
+	void areaCuadrado() {
+		int result = geometria.areacuadrado(4);
+		int expect = 16;
+		assertEquals(expect, result);
+	}
+	
 }
