fa822f0d079f244a431725eb307fd29c1769788a
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-05 09:49:36 +0200

    Geometria Test

--- Junit09_Geometria/src/test/java/JUnit/TestJunit09_Geometria/GeometriaTest.java
+++ Junit09_Geometria/src/test/java/JUnit/TestJunit09_Geometria/GeometriaTest.java
@@ -1,7 +1,11 @@
 package JUnit.TestJunit09_Geometria;
 import org.junit.jupiter.api.Test;
 import JUnit.Junit09_Geometria.dto.Geometria;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GeometriaTest {
-
+	
+	Geometria geo;
+	
+	
 }
