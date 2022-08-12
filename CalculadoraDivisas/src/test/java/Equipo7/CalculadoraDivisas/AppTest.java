9222855105b9331186584ab2a6fec39524955431
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-12 18:52:00 +0200

    Tests

--- CalculadoraDivisas/src/test/java/Equipo7/CalculadoraDivisas/AppTest.java
+++ CalculadoraDivisas/src/test/java/Equipo7/CalculadoraDivisas/AppTest.java
@@ -10,10 +10,38 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
+import Equipo7.Funcionalidad.GestorEventos;
+import Equipo7.Funcionalidad.Methods;
+import Equipo7.Interfaz.InterfazGrafica;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-    
+	GestorEventos gestorEventos;
+	Methods methods;
+	InterfazGrafica ig;
+	
+	@BeforeEach
+	public void before() {
+		methods = new Methods();
+		ig = new InterfazGrafica();
+		gestorEventos = new GestorEventos(ig, methods);
+	}
+	
+	@Test
+	void moneda1() {
+		String result = methods.valorMoneda("moneda1", 1, , null, null)
+	}
+	
+	@Test
+	void valorMoneda() {
+		
+	}
+	
+	@Test
+	void monedaTransform() {
+		
+	}
 }
