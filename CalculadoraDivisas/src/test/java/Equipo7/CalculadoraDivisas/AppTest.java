7051f98ceac9dbe86f71485e67b64827050d8ff2
FelipeGomezGarcia <feli.goga@gmail.com>
2022-08-12 17:39:07 +0200

    Imports to tests

--- CalculadoraDivisas/src/test/java/Equipo7/CalculadoraDivisas/AppTest.java
+++ CalculadoraDivisas/src/test/java/Equipo7/CalculadoraDivisas/AppTest.java
@@ -1,38 +1,19 @@
 package Equipo7.CalculadoraDivisas;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+    
 }
